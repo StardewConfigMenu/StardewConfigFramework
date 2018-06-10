802405a53acb0d1c3c07f5357b07f5faeb564b29
Justin Oroz <justinoroz@me.com>
2018-06-10 16:39:24 -0700

    Revert "Remove Framework"
    This reverts commit 6cee9cf6e4413d8d68c796035adfb8069641d715.

---
+++ StardewConfigFramework/ModOptionModels/ModOptionCategoryLabel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace StardewConfigFramework {
+	public class ModOptionCategoryLabel: ModOption {
+		public ModOptionCategoryLabel(string identifier, string label) : base(identifier, label) {
+
+		}
+	}
+}
