d1f5d6f844518e3bca031f2811d827b4ee931a0c
Justin Oroz <justinoroz@me.com>
2018-06-25 19:29:26 -0700

    Added project for Unit Tests

---
+++ StardewConfigFrameworkTests/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+using StardewConfigFramework;
+
+namespace StardewConfigFrameworkTests {
+	[TestFixture]
+	public class Test {
+		[Test]
+		public void TestCase() {
+		}
+	}
+}
