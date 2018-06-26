68821bc461ec05f3496bfffe819772bcc5633061
Justin Oroz <justinoroz@me.com>
2018-06-25 19:13:12 -0700

    Remove autogenerated noise

--- StardewConfigFramework/OptionsTab.cs
+++ StardewConfigFramework/OptionsTab.cs
@@ -14,16 +14,16 @@ namespace StardewConfigFramework {
 
 		public string Label;
 
-		public int Count => ((IList<ModOption>) Options).Count;
+		public int Count => Options.Count;
 
-		public bool IsReadOnly => ((IList<ModOption>) Options).IsReadOnly;
+		public bool IsReadOnly => Options.IsReadOnly;
 
-		public ICollection<string> Keys => ((IDictionary<string, ModOption>) Options).Keys;
+		public ICollection<string> Keys => Options.Keys;
 
-		public ICollection<ModOption> Values => ((IDictionary<string, ModOption>) Options).Values;
+		public ICollection<ModOption> Values => Options.Values;
 
-		public ModOption this[string key] { get => ((IDictionary<string, ModOption>) Options)[key]; set => ((IDictionary<string, ModOption>) Options)[key] = value; }
-		public ModOption this[int index] { get => ((IList<ModOption>) Options)[index]; set => ((IList<ModOption>) Options)[index] = value; }
+		public ModOption this[string key] { get => Options[key]; set => Options[key] = value; }
+		public ModOption this[int index] { get => Options[index]; set => Options[index] = value; }
 
 		public T GetOption<T>(string identifier) where T : ModOption {
 			return this[identifier] as T;
@@ -43,23 +43,23 @@ namespace StardewConfigFramework {
 		}
 
 		public int IndexOf(ModOption option) {
-			return ((IList<ModOption>) Options).IndexOf(option);
+			return Options.IndexOf(option);
 		}
 
 		public void Insert(int index, ModOption option) {
-			((IList<ModOption>) Options).Insert(index, option);
+			Options.Insert(index, option);
 		}
 
 		public void RemoveAt(int index) {
-			((IList<ModOption>) Options).RemoveAt(index);
+			Options.RemoveAt(index);
 		}
 
 		public void Add(ModOption option) {
-			((IList<ModOption>) Options).Add(option);
+			Options.Add(option);
 		}
 
 		public void Clear() {
-			((IList<ModOption>) Options).Clear();
+			Options.Clear();
 		}
 
 		public bool Contains(string identifier) {
@@ -67,55 +67,55 @@ namespace StardewConfigFramework {
 		}
 
 		public bool Contains(ModOption option) {
-			return ((IList<ModOption>) Options).Contains(option);
+			return Options.Contains(option);
 		}
 
 		public void CopyTo(ModOption[] array, int arrayIndex) {
-			((IList<ModOption>) Options).CopyTo(array, arrayIndex);
+			Options.CopyTo(array, arrayIndex);
 		}
 
 		public bool Remove(ModOption option) {
-			return ((IList<ModOption>) Options).Remove(option);
+			return Options.Remove(option);
 		}
 
 		public IEnumerator<ModOption> GetEnumerator() {
-			return ((IList<ModOption>) Options).GetEnumerator();
+			return Options.GetEnumerator();
 		}
 
 		IEnumerator IEnumerable.GetEnumerator() {
-			return ((IList<ModOption>) Options).GetEnumerator();
+			return Options.GetEnumerator();
 		}
 
 		public void Add(string key, ModOption value) {
-			((IDictionary<string, ModOption>) Options).Add(key, value);
+			Options.Add(key, value);
 		}
 
 		public bool ContainsKey(string key) {
-			return ((IDictionary<string, ModOption>) Options).ContainsKey(key);
+			return Options.ContainsKey(key);
 		}
 
 		public bool Remove(string key) {
-			return ((IDictionary<string, ModOption>) Options).Remove(key);
+			return Options.Remove(key);
 		}
 
 		public bool TryGetValue(string key, out ModOption value) {
-			return ((IDictionary<string, ModOption>) Options).TryGetValue(key, out value);
+			return Options.TryGetValue(key, out value);
 		}
 
 		public void Add(KeyValuePair<string, ModOption> option) {
-			((IDictionary<string, ModOption>) Options).Add(option);
+			Options.Add(option);
 		}
 
 		public bool Contains(KeyValuePair<string, ModOption> option) {
-			return ((IDictionary<string, ModOption>) Options).Contains(option);
+			return Options.Contains(option);
 		}
 
 		public void CopyTo(KeyValuePair<string, ModOption>[] array, int arrayIndex) {
-			((IDictionary<string, ModOption>) Options).CopyTo(array, arrayIndex);
+			Options.CopyTo(array, arrayIndex);
 		}
 
 		public bool Remove(KeyValuePair<string, ModOption> option) {
-			return ((IDictionary<string, ModOption>) Options).Remove(option);
+			return Options.Remove(option);
 		}
 
 		IEnumerator<KeyValuePair<string, ModOption>> IEnumerable<KeyValuePair<string, ModOption>>.GetEnumerator() {
