b7bda1e5e1815aea2bafed965ec861b6b8138e8d
Justin Oroz <justinoroz@me.com>
2018-06-25 23:30:38 -0700

    OptionTab Tests

--- StardewConfigFrameworkTests/OptionTabTests.cs
+++ StardewConfigFrameworkTests/OptionTabTests.cs
@@ -28,5 +28,48 @@ namespace StardewConfigFrameworkTests {
 
 			Assert.Catch(() => Tab.Add(option1dupe));
 		}
+
+		[Test]
+		public void AddingAndRetrieving() {
+			var option1 = new CategoryLabel("option1", "Option 1");
+			var option2 = new CategoryLabel("option2", "Option 2");
+			var option3 = new CategoryLabel("option3", "Option 3");
+
+			Tab.Add(option1);
+			Tab.Add(option2);
+			Tab.Add(option3);
+
+			Assert.AreEqual(Tab["option2"].Label, "Option 2");
+		}
+
+		[Test]
+		public void SubscriptSetting() {
+			var option1 = new CategoryLabel("option1", "Option 1");
+			var option2 = new CategoryLabel("option2", "Option 2");
+			var option3 = new CategoryLabel("option3", "Option 3");
+			var option4 = new CategoryLabel("option4", "Option 4");
+
+			Tab.Add(option1);
+			Tab.Add(option2);
+			Tab.Add(option3);
+
+			Tab[1] = option4;
+
+			Assert.AreEqual(Tab.IndexOf("option4"), 1);
+		}
+
+		[Test]
+		public void SubscriptSettingDuplicateError() {
+			var option1 = new CategoryLabel("option1", "Option 1");
+			var option2 = new CategoryLabel("option2", "Option 2");
+			var option3 = new CategoryLabel("option3", "Option 3");
+			var option3dupe = new CategoryLabel("option3", "Option 3 Dupe");
+
+			Tab.Add(option1);
+			Tab.Add(option2);
+			Tab.Add(option3);
+
+			Assert.Catch(() => Tab[1] = option3dupe);
+		}
 	}
 }
