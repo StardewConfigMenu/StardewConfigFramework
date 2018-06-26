7e1fb0e300158c6b3a65fa6fbb36e7d0ab1bb368
Justin Oroz <justinoroz@me.com>
2018-06-25 20:29:26 -0700

    Created TestManifest

--- StardewConfigFrameworkTests/TestMod.cs
+++ StardewConfigFrameworkTests/TestMod.cs
@@ -9,7 +9,8 @@ namespace StardewConfigFrameworkTests {
 
 		public IMonitor Monitor => throw new NotImplementedException();
 
-		public IManifest ModManifest => throw new NotImplementedException();
+		public IManifest ModManifest => manifest;
+		private TestManifest manifest = new TestManifest();
 
 		public void Entry(IModHelper helper) {
 			throw new NotImplementedException();
@@ -18,5 +19,29 @@ namespace StardewConfigFrameworkTests {
 		public object GetApi() {
 			throw new NotImplementedException();
 		}
+	}
+
+	public class TestManifest: IManifest {
+		public string Name => "Test Mod";
+
+		public string Description => throw new NotImplementedException();
+
+		public string Author => throw new NotImplementedException();
+
+		public ISemanticVersion Version => throw new NotImplementedException();
+
+		public ISemanticVersion MinimumApiVersion => throw new NotImplementedException();
+
+		public string UniqueID => "Test.Mod";
+
+		public string EntryDll => throw new NotImplementedException();
+
+		public IManifestContentPackFor ContentPackFor => throw new NotImplementedException();
+
+		public IManifestDependency[] Dependencies => throw new NotImplementedException();
+
+		public string[] UpdateKeys => throw new NotImplementedException();
+
+		public IDictionary<string, object> ExtraFields => throw new NotImplementedException();
 	}
 }
