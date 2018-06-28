30afb0a1e2be4008e95b58197cf3e8450d3661e9
Justin Oroz <justinoroz@me.com>
2018-06-28 00:52:56 -0700

    IAction

--- StardewConfigFramework/OptionModels/Action/Action.cs
+++ StardewConfigFramework/OptionModels/Action/Action.cs
@@ -1,18 +1,16 @@
 ﻿
 namespace StardewConfigFramework.Options {
 
-	public class Action: ModOption {
-		public delegate void Handler(Action action);
-		public enum ActionType {
-			OK, SET, CLEAR, DONE, GIFT
-		}
+	public class Action: ModOption, IAction {
 
 		public event Handler ActionWasTriggered;
-		public ActionType Type;
 
 		public Action(string identifier, string labelText, ActionType type, bool enabled = true) : base(identifier, labelText, enabled) {
 			Type = type;
 		}
+
+		public ActionType _Type;
+		public ActionType Type { get => _Type; set => _Type = value }
 
 		public void Trigger() {
 			ActionWasTriggered?.Invoke(this);
---
+++ StardewConfigFramework/OptionModels/Action/IAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace StardewConfigFramework.Options {
+	public interface IAction {
+		event Handler ActionWasTriggered;
+		ActionType Type { get; set; }
+		void Trigger();
+	}
+
+	public delegate void Handler(Action action);
+
+	public enum ActionType {
+		OK, SET, CLEAR, DONE, GIFT
+	}
+}
