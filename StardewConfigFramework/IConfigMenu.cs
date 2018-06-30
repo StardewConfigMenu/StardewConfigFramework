a3646ac89b991c560333f76c472e70f6694beb4c
Justin Oroz <justinoroz@me.com>
2018-06-29 20:41:43 -0700

    Use SMAPI API method

--- StardewConfigFramework/IConfigMenu.cs
+++ StardewConfigFramework/IConfigMenu.cs
@@ -3,7 +3,6 @@ namespace StardewConfigFramework {
 	public delegate void ModAddedSettings();
 
 	public abstract class IConfigMenu {
-		public static IConfigMenu Instance { get; protected set; }
 		public abstract void AddOptionsPackage(IOptionsPackage package);
 	}
 }
