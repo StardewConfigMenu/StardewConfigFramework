aa2d354ad2a41dd6597481e6017effc1ecd59bc0
Justin Oroz <justinoroz@me.com>
2018-06-30 11:24:05 -0700

    missing reference

--- StardewConfigFramework/OptionPackages/IOptionsTab.cs
+++ StardewConfigFramework/OptionPackages/IOptionsTab.cs
@@ -1,4 +1,6 @@
-﻿namespace StardewConfigFramework {
+﻿using StardewConfigFramework.Options;
+
+namespace StardewConfigFramework {
 	public interface IOptionsTab {
 		T GetOption<T>(string identifier) where T : IConfigOption;
 		T GetOption<T>(int index) where T : IConfigOption;
